6d5ef63578236822c53ccb31268f483d2f794204
Tukaram Kolekar <tukaramkolekar1@gmail.com>
2022-07-26 18:41:55 +0530

    Added article listing

--- config/routes.rb
+++ config/routes.rb
@@ -2,7 +2,7 @@ Rails.application.routes.draw do
 
   root 'application#hello'
 
-  resources :articles , only: [:show]
+  resources :articles, only: [:show, :index]
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Defines the root path route ("/")
