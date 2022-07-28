49a8470e43878330dbfbfe9feebb3b58314c1df6
Tukaram Kolekar <tukaramkolekar1@gmail.com>
2022-07-28 12:41:10 +0530

    article create action added

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -3,4 +3,27 @@ class ArticlesController < ApplicationController
         #binding.break #for debuging the code to read params passed
         @article =Article.find(params[:id])
     end
+
+    def index
+        @articles =Article.all
+    end
+
+    def new
+       @article=Article.new
+    end
+
+    def create
+        #@article = Article.new(params.require(:article).permit(:title, :description))
+     
+        @article = Article.new(article_params)
+         @article.save
+         redirect_to articles_show(@article)
+       end
+      private
+        def article_params
+         params.require(:article).permit(:title, :description)
+ 
+         end
+ 
+
 end
