c9f6d833e9ed187cc81dd104c88784e7acaf76a4
Tukaram Kolekar <tukaramkolekar1@gmail.com>
2022-08-02 12:15:22 +0530

    Delete method started to function using turbo_method

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -41,16 +41,17 @@ class ArticlesController < ApplicationController
             render 'new'
        end
     end
+
+    def destroy
+        @article.destroy
+        redirect_to root_path
+        end
+
       private
         def article_params
          params.require(:article).permit(:title, :description)
         end
 
-        def destroy
-        @article=Article.find(params[:id])
-        @article.destroy
-        redirect_to articles_path
-        end
         def set_article
             @article = Article.find(params[:id])
          end
